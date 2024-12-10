18acfce2eeabe4fb362008bc4a75ca533284d18a
Hafsa-Begum <86627326+Hafsa-Begum@users.noreply.github.com>
2024-12-10 23:03:14 +0600

    add simple header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="shadow-md text-gray-600">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        {/* Logo */}
+        <div className="text-2xl font-bold">
+          <Link className="hover:text-gray-300" href="/">
+            Logo
+          </Link>
+        </div>
+
+        {/* Navigation Links */}
+        <nav>
+          <ul className="flex space-x-6">
+            <li>
+              <Link className="hover:text-gray-300" href="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-gray-300" href="/services">
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-gray-300" href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
